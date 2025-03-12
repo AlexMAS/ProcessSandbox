d4e502531c5d0cf0335351588e32c20b1f5899a5
Alexander Mezhov <as.mezhov@gmail.com>
2025-03-12 16:46:32 +0500

    fix: Fix searching sandbox-exec (v0.3.0) (#2)

--- build.sh
+++ build.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
-set -e
-
 cd $(dirname "$0")
 
 rm -rf ProcessSandbox/bin/publish/ 2> /dev/null
 rm -rf ProcessSandbox.App/bin/publish/ 2> /dev/null
+
+mkdir -p ProcessSandbox/bin/publish/
+mkdir -p ProcessSandbox.App/bin/publish/
+
+set -e
 
 docker build --file build.Dockerfile -t sandbox:latest .
 id=$(docker create sandbox:latest)
